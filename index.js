1d1b66ccdc7cbe393420afc42ee1b125da34ee71
devrafsun10 <ssiyam152@gmail.com>
2026-02-28 13:09:05 +0600

    structure

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://mern2408:hrmanagment@cluster0.qxv1zbq.mongodb.net/hrmanagment?appName=Cluster0').then(() => {
+    console.log("connect to mongodb");
+
+})
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+})
+
+//mongodb+srv://mern2408:hrmanagment@cluster0.qxv1zbq.mongodb.net/hrmanagment?appName=Cluster0
+
+app.listen(8000, () => {
+    console.log("server is running on port 8000");
+
+})
