9056fcfa3545abee712b8b656aecbe8bd6dbf520
devrafsun10 <ssiyam152@gmail.com>
2026-04-04 09:53:40 +0600

    bcrypt password using .hashSync

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,24 +19,29 @@ let registrationController = async (req,res) => {
         })
     }//we used return statement to stop the execution of the function if the user with the same email already exists in the database and send a response to the frontend with a message that the user with this email already exists
 
-     bcrypt.hash(password, 10, function (err, hash) {
+    const hash = bcrypt.hashSync( password, 10); //for hashing the password synchronously with a salt rounds of 10
 
-        if (err) {
-            console.log(err);
+    console.log("hashed password", hash);
+    
+
+//      bcrypt.hash(password, 10, function (err, hash) {
+
+//         if (err) {
+//             console.log(err);
             
-            return res.status(500).json({
-                success: false,
-                message: "server error"
-            })
-        }
-        console.log("pass",hash);
+//             return res.status(500).json({
+//                 success: false,
+//                 message: "server error"
+//             })
+//         }
+//         console.log("pass",hash);
         
-  });//we used bcrypt to hash the password before saving it to the database for security reasons
+//   });//we used bcrypt to hash the password before saving it to the database for security reasons
 
      let createUser = new User({
         username: username,
         email: email,
-        password: password,
+        password: hash,
     })
     
     createUser.save();//for saving the user to the database
